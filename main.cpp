82ac386427290c47fe6331c5c694e156026cd0ff
Sinabb <86355292+Sinabb@users.noreply.github.com>
2023-07-15 01:11:17 +0900

    BaekJoon2

--- main.cpp
+++ main.cpp
@@ -2,10 +2,28 @@
 
 int main()
 {
+	int a;
+	int b;
+	int c;
+
+	std::cin >> a,std::cin >> b;
+
+	c = a + b;
+	std::cout << c << std::endl;
 	
 }
 
 /* 1번 Hello World!를 출력하세요
 * 
 std::cout << "Hello World!" << std::endl;
+
+2번 두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오.
+	int a;
+	int b;
+	int c;
+
+	std::cin >> a,std::cin >> b;
+
+	c = a + b;
+	std::cout << c << std::endl;
 */ 
