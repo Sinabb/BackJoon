0d7b441d470b6119562d485a614e94d658af5e52
Sinabb <86355292+Sinabb@users.noreply.github.com>
2023-07-28 20:31:53 +0900

    10430번 문제

--- main.cpp
+++ main.cpp
@@ -2,16 +2,27 @@
 
 int main()
 {
-	const int a = 100;
-	char Print[a];
+	int A, B, C;
+	std::cin >> A >> B >> C;
 
-	std::cin.getline(Print, a);
+	int result1 = (A + B) % C;
 
-	std::cout << Print << "??!" << std::endl;
+	int result2 = ((A % C) + (B % C)) % C;
+
+	int result3 = (A * B) % C;
+
+	int result4 = ((A % C) * (B % C)) % C;
+
+	std::cout << result1 << std::endl;
+	std::cout << result2 << std::endl;
+	std::cout << result3 << std::endl;
+	std::cout << result4 << std::endl;
+
+	return 0;
 }
 
 /* 1번 Hello World!를 출력하세요
-* 
+
 std::cout << "Hello World!" << std::endl;
 
 2번 두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오.
@@ -48,4 +59,51 @@ int main()
 	}
 
 }
+*/
+/*
+	10926번 문제
+	const int a = 100;
+	char Print[a];
+
+	std::cin.getline(Print, a);
+
+	std::cout << Print << "??!" << std::endl;
+*/
+
+/*
+	18108번
+	#include <iostream>
+
+int main()
+{
+	int Year;
+	int tYear;
+
+	std::cin >> tYear;
+	
+	Year = tYear - 543;
+	
+	std::cout << Year << std::endl;
+}
+*/
+
+/*
+	10430번 문제
+		int A, B, C;
+	std::cin >> A >> B >> C;
+
+	int result1 = (A + B) % C;
+
+	int result2 = ((A % C) + (B % C)) % C;
+
+	int result3 = (A * B) % C;
+
+	int result4 = ((A % C) * (B % C)) % C;
+
+	std::cout << result1 << std::endl;
+	std::cout << result2 << std::endl;
+	std::cout << result3 << std::endl;
+	std::cout << result4 << std::endl;
+
+	return 0;
 */
