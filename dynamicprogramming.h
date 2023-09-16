e8cddd7b19428b8bc107cc1e96efe9d8108c2f78
Sinabb <86355292+Sinabb@users.noreply.github.com>
2023-09-16 23:30:02 +0900

    백준 다이나믹 프로그래밍 1463번  1로 만들기

--- dynamicprogramming.h
+++ dynamicprogramming.h
@@ -1,6 +1,8 @@
 #pragma once
 
 #include <iostream>
+#include <vector>
+#include <algorithm>
 
 int Dynamic2839()
 {
@@ -22,4 +24,23 @@ int Dynamic2839()
         std::cout << -1 << std::endl;
         return 0;
     }
+}
+
+
+
+int  Dynamic1463(){
+    int N;
+    std::cin >> N;
+
+    std::vector<int> dp(N + 1, 0);
+
+    for (int i = 2; i <= N; i++) {
+        dp[i] = dp[i - 1] + 1; 
+        if (i % 2 == 0) dp[i] = std::min(dp[i], dp[i / 2] + 1); 
+        if (i % 3 == 0) dp[i] = std::min(dp[i], dp[i / 3] + 1); 
+    }
+
+    std::cout << dp[N] << std::endl;
+
+    return 0;
 }
